12b5a634685a667ff0ed535148ad4d82bd9213cb
cootcode <dawid.grabowski19@gmail.com>
2023-11-12 17:39:40 +0000

    Add project files.

---
+++ SpitTree_MVC/Models/SpitTreeDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpitTree_MVC.Models
+{
+        public class SpitTreeDbContext : IdentityDbContext<User>
+        {
+            public SpitTreeDbContext()
+                : base("DefaultConnection", throwIfV1Schema: false)
+            {
+            }
+
+            public static SpitTreeDbContext Create()
+            {
+                return new SpitTreeDbContext();
+            }
+        }
+}
---
+++ SpitTree_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SpitTree_MVC.Startup))]
+namespace SpitTree_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
